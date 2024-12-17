6d9911fb1d606843ae419341b47f8dd2a4ecba34
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-17 17:09:56 +0530

    [Pooja] added: Finding Maximum Number

---
+++ FindMaximum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Practice
+{
+    internal class FindMaximum
+    {
+        public static T GetMax<T>(T  a, T b, T c) where T: IComparable<T>
+        {
+            T max;
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+            {
+                max = a;
+                Console.WriteLine($"The maximum number amoung 3 is {max}");
+            }
+            else if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+            {
+                max = b;
+                Console.WriteLine($"The maximum number is {max}");
+            }
+            else
+            {
+                max = c;
+                Console.WriteLine($"The maximum number is {max}");
+            }
+            return max;
+        }
+    }
+}
