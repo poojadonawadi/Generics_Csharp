8a0539df9ba535793e39bd9c2f43590d19043d88
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-17 17:14:02 +0530

    [Pooja]: added PrintArray

---
+++ PrintArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Practice
+{
+    //Displaying elements without using Generics
+    internal class PrintArray
+    {
+        public static void ToPrint(int[] intArray)
+        {
+            foreach(int elements in intArray)
+            {
+                Console.WriteLine(elements);
+            }
+        }
+        public static void ToPrint(double[] doubleArray)
+        {
+            foreach (double elements in doubleArray)
+            {
+                Console.WriteLine(elements);
+            }
+        }
+        public static void ToPrint(char[] charArray)
+        {
+            foreach (char elements in charArray)
+            {
+                Console.WriteLine(elements);
+            }
+        }
+    }
+}
